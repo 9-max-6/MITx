8764e9987650470783136bc5176d0f57ca1abecf
Maxwell Mutuku <85401959+9-max-6@users.noreply.github.com>
2024-07-09 11:02:42 +0300

    Fix - deleted lbuild.sh that was pushed to the repo from the server

--- lbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-sudo cp landing-page/build/index.html api/templates/
-sudo cp -r landing-page/build/static/css/* /var/www/staticfiles/css
-sudo cp -r landing-page/build/static/js/* /var/www/staticfiles/js
-sudo systemctl restart nginx
-sudo systemctl restart gunicorn
