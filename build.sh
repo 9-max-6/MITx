67ec933fd475bd595eedfb251639bcc58680d446
Maxwell Mutuku <85401959+9-max-6@users.noreply.github.com>
2024-07-09 11:02:05 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-sudo cp dashboard/build/index.html api/templates/dashboard/
-sudo cp -r dashboard/build/static/css/* /var/www/staticfiles/css
-sudo cp -r dashboard/build/static/js/* /var/www/staticfiles/js
-sudo cp -r dashboard/build/static/media/* /var/www/staticfiles/media
-
-sudo systemctl restart nginx
-sudo systemctl restart gunicorn
